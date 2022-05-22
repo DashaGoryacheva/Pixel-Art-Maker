2613b84a8c70bd9a86900be7675d2bfd950eaf0d
Dasha <daryagoryacheva94@gmail.com>
2022-05-22 20:12:27 +0300

    rev1

--- designs.js
+++ designs.js
@@ -1,28 +1,46 @@
 // Select color input
 // Select size input
 // When size is submitted by the user, call makeGrid()
-//var userColor= document.querySelector ('#colorPicker');
+const userColor = document.querySelector('#colorPicker');
 const tblHead = document.createElement('thead');
 const tblBody = document.createElement('tbody');
 const tbl = document.querySelector('#pixelCanvas');
 const formInput = document.querySelectorAll('input');
 const thirdInput = formInput[2];
 const form = document.querySelector('#sizePicker');
-const y = form.elements['inputWidth'];
-const x = form.elements['inputHeight'];
+const y = form.elements['height'];
+const x = form.elements['width'];
 let yVal = y.value;
 let xVal = x.value;
-
+let numClick=0;
 tbl.appendChild(tblHead);
 tbl.appendChild(tblBody);
+thirdInput.addEventListener('click', countClick);
+function countClick(){
+numClick++;
+};
 thirdInput.addEventListener('click', makeGrid);
 function makeGrid(event) {
-    for (let i = 1; i <= xVal; i++) {
-        let column = document.createElement('tr');
-        for (let j = 1; j <= yVal; j++) {
-            let row = document.createElement('td');
-            column.appendChild(row);
+    if (numClick%2 !== 0) {
+        event.preventDefault();
+        yVal = y.value;
+        xVal = x.value;
+        for (let i = 1; i <= xVal; i++) {
+            let column = document.createElement('tr');
+            for (let j = 1; j <= yVal; j++) {
+                let row = document.createElement('td');
+                column.appendChild(row);
+            }
+            tblBody.appendChild(column);
         }
-        tblBody.appendChild(column);
+    }
+    else {
+        thirdInput.removeEventListener('click', makeGrid);
+    }
+}
+tblBody.addEventListener('click', userBkground);
+function userBkground(e) {
+    if (e.target.nodeName === 'TD') {
+        e.target.style.backgroundColor = userColor.value;
     }
 }
